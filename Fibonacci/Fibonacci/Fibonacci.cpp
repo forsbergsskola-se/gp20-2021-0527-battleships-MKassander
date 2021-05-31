725539912239df9680896f07dfb5d8cc6c8080c1
Max Kassander <maximilian.kassander@gmail.com>
2021-05-31 16:12:39 +0200

    Adds fibonacci

---
+++ Fibonacci/Fibonacci/Fibonacci.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
+/*
+Fibonacci(n) is defined as:
+Fibonacci(0) = 0
+Fibonacci(1) = 1
+Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
+Implement this function using recursion
+Implement this function using iteration
+Iteration defines an algorithm that is implemented using iterations (loops)
+Rather than recursive (self-calling) function calls
+Optional:
+
+Try to compare the performance of each implementation
+Can you notice a difference?
+How do you explain that?
+*/
