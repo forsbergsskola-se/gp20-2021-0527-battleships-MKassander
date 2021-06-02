b5fd3393f3c5df633d4e2c2e50db381842f4d7a4
Max Kassander <maximilian.kassander@gmail.com>
2021-06-02 22:35:20 +0200

    Adds battleships

---
+++ Battleships/Battleships/Battleships.cpp
@@ -0,0 +1,29 @@
+using namespace std;
+
+#include <iostream>
+
+int main()
+{
+    
+}
+
+/*
+The Rules:
+2 Players
+
+10 x 10 grid per player
+
+Ships: sizes 5, 4, 3, 2 ships with 2
+
+Ships may be placed vertically or horizontally
+
+Ships may not overlap and may not touch each other (i.e. there needs to be at least 1 empty grid cell around the ship in every direction)
+
+Players take alternating turns
+
+Coordinates given in the form g7, a2, d2 etc...
+
+Feedback "Hit" or "Miss"
+
+Feedback "Ship sunk!"
+*/
