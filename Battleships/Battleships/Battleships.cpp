689a514cb1215ea543564f2ef5a0454beaa25fdd
Max Kassander <maximilian.kassander@gmail.com>
2021-06-02 23:09:35 +0200

    A grid and placing on a cell

--- Battleships/Battleships/Battleships.cpp
+++ Battleships/Battleships/Battleships.cpp
@@ -4,7 +4,32 @@ using namespace std;
 
 int main()
 {
-    
+	string grid[100] = {};
+	fill(begin(grid), begin(grid) + 100, "x");
+
+	while (true)
+	{
+		int row = 0;
+		for (int i = 0; i < 100; i++)
+		{
+			cout << grid[i] << " ";
+			++row;
+			if (row == 10)
+			{
+				cout << endl;
+				row = 0;
+			}
+		}
+
+		cout << "Enter cell to place on:" << endl;
+		int i;
+		cin >> i;
+
+		grid[i] = "O";
+
+		cout << "Enter direction:" << endl;
+		cin >> i;
+	}
 }
 
 /*
