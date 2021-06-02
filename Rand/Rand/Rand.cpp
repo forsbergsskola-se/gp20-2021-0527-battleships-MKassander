b2794566c373d7e525905bc9fa868304211d98eb
Max Kassander <maximilian.kassander@gmail.com>
2021-06-02 22:30:23 +0200

    rand asignment

--- Rand/Rand/Rand.cpp
+++ Rand/Rand/Rand.cpp
@@ -1,20 +1,38 @@
-// Rand.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+using namespace std;
 
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Enter number of iterations:" << endl;
+    float iterations;
+    cin >> iterations;
+
+    float pool[20] = {};
+    int r;
+
+    srand(time(0));
+
+    for (int i = 0; i <= iterations; i++)
+    {
+        r = rand() % 20 + 1;
+        pool[r - 1] += 1;
+    }
+    cout << endl;
+    for (int i = 0; i < 20; i++)
+    {
+        float proc = (pool[i] / iterations) * 100;
+        cout << i +1 << ": " << pool[i] << " (" << proc << " %)" << endl;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+/*
+Test the rand() function!
+Create a large number of random numbers between 1 and 20!
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+Count how often every number appears
+
+Compare this to the ideal distribution (i.e. display propability of every number or something like that)
+*/
