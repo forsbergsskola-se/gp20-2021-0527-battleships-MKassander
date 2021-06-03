a993da124ecc3525cb5532b495659f76440eb45f
Max Kassander <maximilian.kassander@gmail.com>
2021-06-03 09:48:20 +0200

    Adds coordinates

--- Battleships/Battleships/Battleships.cpp
+++ Battleships/Battleships/Battleships.cpp
@@ -2,24 +2,19 @@ using namespace std;
 
 #include <iostream>
 
+
+void DisplayGrid(string grid[]);
+
 int main()
 {
 	string grid[100] = {};
 	fill(begin(grid), begin(grid) + 100, "x");
 
+
+
 	while (true)
 	{
-		int row = 0;
-		for (int i = 0; i < 100; i++)
-		{
-			cout << grid[i] << " ";
-			++row;
-			if (row == 10)
-			{
-				cout << endl;
-				row = 0;
-			}
-		}
+		DisplayGrid(grid);
 
 		cout << "Enter cell to place on:" << endl;
 		int i;
@@ -30,6 +25,27 @@ int main()
 		cout << "Enter direction:" << endl;
 		cin >> i;
 	}
+}
+
+void DisplayGrid(string grid[]) {
+	cout << endl;
+	int row = 0;
+	int rowNumber = 0;
+	cout << "  a b c d e f g h i j" << endl;
+	cout << rowNumber << " ";
+	for (int i = 0; i < 100; i++)
+	{
+		cout << grid[i] << " ";
+		++row;
+		if (row == 10)
+		{
+			cout << endl;
+			row = 0;
+			if (rowNumber < 9)
+				cout << ++rowNumber << " ";
+		}
+	}
+	cout << endl;
 }
 
 /*
